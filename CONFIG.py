7b52694cb47bade6b3ae455400610b9a78e95328
Levente <levente.kobor94@gmail.com>
2018-09-15 23:24:31 +0200

    This Is Why We Don't Push To Production On Fridays

---
+++ CONFIG.py
@@ -0,0 +1,10 @@
+ITERATIONS = 1000
+WIDTH, HEIGHT = 601, 601  
+DIAMETER = 1
+
+start = (-.5 - DIAMETER / 2, 0)  
+d_over_h = DIAMETER / HEIGHT
+d_over_w = DIAMETER / WIDTH
+
+real, imag = start
+colors = ("#FFF", "#111111", "#444444", "#000000")
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,13 +1,35 @@
 from tkinter import *
+import CONFIG
 
-def setup():
+def mandelbrot(c):
+    z = 0
+    for i in range(CONFIG.ITERATIONS):
+        z = z*z + c
+        if abs(z) > 2:
+            return i     
+    return CONFIG.ITERATIONS
+
+def create_canvas():
     root = Tk()
-    root.geometry('500x500')
-
-    canvas = Canvas(root, height = 500, width = 500)
+    canvas = Canvas(root, width=CONFIG.WIDTH,height=CONFIG.HEIGHT)
+    root.title("mandelbrot")
     canvas.pack()
+    img = PhotoImage(width=CONFIG.WIDTH, height=CONFIG.HEIGHT)
+    canvas.create_image(((CONFIG.WIDTH+1)//2, (CONFIG.HEIGHT+1)//2), image=img, state="normal")
 
-    root.mainloop()
+def draw():
+    create_canvas()
+    for x in range(CONFIG.WIDTH):
+        for y in range(CONFIG.HEIGHT//2, -1, -1):
+            i = mandelbrot(complex(real, imag))
+            color = colors[i//2 % (len(colors))]
+            img.put(color, (x, y))
+            img.put(color, (x, (CONFIG.HEIGHT - 1) - y)) 
+            imag += CONFIG.d_over_h
+        CONFIG.imag = CONFIG.start[1]
+        CONFIG.real += CONFIG.d_over_w
+
+    mainloop()
 
 if __name__ == '__main__':
-    setup()
+    draw()
